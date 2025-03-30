06983335f545da8fcb2a8e906860eb9b411816c7
yoonyesol <maniac970430@naver.com>
2025-03-30 22:13:09 +0900

    Chore: sitemap 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: "https://just-meow-it.timeqlife.com" || "http://localhost:3000",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: "daily",
+    priority: 1,
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: "*",
+                allow: "/",
+            },
+        ],
+    },
+};
