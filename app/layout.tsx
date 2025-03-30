e22afd3da967bc92279e0705f4a60a2fe133f54f
yoonyesol <maniac970430@naver.com>
2025-03-30 14:09:43 +0900

    Feat : 헤더 추가 및 CSS 스타일링

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <head />
       <body className="bg-gray-100">
+        <header className='flex fixed top-0 px-3 left-0 w-full h-16 bg-gray-800 text-white z-10'>
+          <h1 className="flex items-center text-3xl font-bold">🐱 고양이의 조언</h1></header>
         <div className="container mx-auto">{children}</div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import MeowButton from '@/components/MeowButton';
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-3xl font-bold">🐱 고양이의 조언</h1>
       <CatModel />
       <MeowButton />
     </main>
