39389c60934bd5da07f41fb6704b9a075f817e3b
yoonyesol <maniac970430@naver.com>
2025-03-30 12:35:01 +0900

    Config: prettier, eslint 환경설정 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,103 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import vitest from 'eslint-plugin-vitest';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')];
-
-export default eslintConfig;
+export default defineConfig([
+  // JavaScript 및 TypeScript 파일을 위한 설정
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  
+  // 언어 옵션과 브라우저 글로벌 변수 설정
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], 
+    languageOptions: { globals: { ...globals.browser, ...vitest.environments.env.globals } }
+  },
+  
+  // 추천 ESLint 규칙 및 플러그인 확장
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { 
+      js, 
+      '@typescript-eslint': tseslint,
+      react: pluginReact, 
+      vitest: vitest,
+      prettier 
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'airbnb',
+      'airbnb/hooks',
+      'airbnb-typescript',
+      'plugin:@tanstack/eslint-plugin-query/recommended',
+      'plugin:testing-library/react',
+      'plugin:vitest/recommended',
+      'prettier'
+    ],
+  },
+  
+  // 규칙 설정
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'arrow-body-style': 'off',
+      'consistent-return': 'off',
+      'object-curly-newline': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-shadow': 'warn',
+      '@typescript-eslint/no-use-before-define': 'off',
+      'no-param-reassign': 'warn',
+      'no-return-assign': 'warn',
+      'no-unused-vars': 'warn',
+      'no-cond-assign': 'off',
+      'no-plusplus': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react/require-default-props': 'off',
+      'react/no-array-index-key': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/jsx-props-no-spreading': 'off',
+      'import/prefer-default-export': 'warn',
+      'import/no-extraneous-dependencies': 'off',
+      'import/extensions': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'warn',
+      'vitest/valid-title': 'off',
+      'jsx-a11y/label-has-associated-control': [
+        2,
+        { labelAttributes: ['htmlFor'] },
+      ],
+    },
+  },
+  
+  // 무시할 파일 목록
+  { 
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    ignorePatterns: [
+      'dist', 'build', 'node_modules', 'public', '.history', '.eslintrc.cjs',
+      'yarn.lock', 'postcss.config.js', 'tailwind.config.js',
+    ],
+  },
+  
+  // 파서 설정
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true },
+      project: './tsconfig.json',
+    },
+  },
+  
+  // React 설정
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+]);
