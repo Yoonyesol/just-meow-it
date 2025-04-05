019845bb0ee577ce9feaf9ba849a422ba3870020
yoonyesol <maniac970430@naver.com>
2025-04-05 23:15:38 +0900

    Feat: 구글 애드센스 코드 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import AdSense from '@/components/AdSense';
 
 export const metadata: Metadata = {
   title: 'Just Meow It | 고양이의 조언으로 고민 해결하기',
@@ -18,13 +19,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <head>
         <meta name="naver-site-verification" content="6f3a93abd49e8da4b70f3107ebdf3890e0ab142b" />
+        <script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.GOOGLE_ADS_KEY}`}
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="bg-gray-100 flex flex-col h-full overflow-hidden">
         <header className='flex fixed top-0 px-3 left-0 w-full h-16 bg-gray-800 text-white z-10'>
           <h1 className="flex items-center text-2xl font-bold font-bnviit">🐱 Just Meow It</h1>
         </header>
         <div className="container mx-auto pt-16 flex-1">
+          <AdSense adSlot={"8185832302"} />
           {children}
+          <AdSense adSlot={"8185832302"} />
         </div>
       </body>
     </html>
---
+++ components/AdSense.js
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+const AdSense = ({ adSlot }) => {
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-layout-key={process.env.GOOGLE_ADS_LAYOUT_KEY}
+      data-ad-client={process.env.GOOGLE_ADS_KEY}
+      data-ad-slot={adSlot}
+      data-ad-format="fluid"
+      data-full-width-responsive="true"
+    ></ins>
+  );
+};
+
+export default AdSense;
