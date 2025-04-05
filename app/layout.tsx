e0f82b520c2dad629b2de47ea026751707ef07d9
yoonyesol <maniac970430@naver.com>
2025-04-05 23:45:01 +0900

    UI: overflow-hidden 속성 제거 및 메타태그 키워드 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import AdSense from '@/components/AdSense';
 export const metadata: Metadata = {
   title: 'Just Meow It | 고양이의 조언으로 고민 해결하기',
   description: '고양이의 지혜를 통해 고민을 해결해 보세요.',
-  keywords: '고양이, 조언, 고민, 팁, 동물, 심리 상담, 동물 조언, 정신 건강',
+  keywords: '고양이, 고민, 해결, 고민해결, 해결의책, 고민, 팁, 동물, 심리 상담, 동물 조언, 정신 건강',
   robots: 'index, follow',
   openGraph: {
     title: 'Just Meow It | 고양이의 조언으로 고민 해결하기',
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           crossOrigin="anonymous"
         />
       </head>
-      <body className="bg-gray-100 flex flex-col h-full overflow-hidden">
+      <body className="bg-gray-100 flex flex-col h-full">
         <header className='flex fixed top-0 px-3 left-0 w-full h-16 bg-gray-800 text-white z-10'>
           <h1 className="flex items-center text-2xl font-bold font-bnviit">🐱 Just Meow It</h1>
         </header>
