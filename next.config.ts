636279af896c7d7ac46f6066b5965397a266ce22
yoonyesol <maniac970430@naver.com>
2026-05-02 23:01:25 +0900

    feat: configure project for Toss App-in deployment and build environment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  distDir: '.next/web', // 토스 CLI가 빌드 결과물을 찾는 경로
 };
 
 export default nextConfig;
